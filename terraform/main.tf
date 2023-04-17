2db3269d81e55e7e8a351523dc9355ed3390b26f
PavloAn <91308922+PavloAn@users.noreply.github.com>
2023-04-17 15:45:03 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
-terraform {
-  required_providers {
-    virtualbox = {
-      source = "terra-farm/virtualbox"
-      version = "0.2.2-alpha.1"
+resource "virtualbox_vm" "example" {
+  name        = "my-vm" # Назва машини
+  image       = "https://vagrantcloud.com/ubuntu/boxes/xenial64/versions/20180420.0.0/providers/virtualbox.box"  # Образ який потрібно використовувати
+  memory      = "1024 mib" 
+  cpus        = "1"
+  boot_order  = ["disk"]
+
+  network_adapter {
+       type = "bridged"
+       host_interface="en0"
     }
-  }
 }
